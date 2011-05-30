5e0c489cf7fccc0d6359a6dc4877a5438acd7210
d-kami <kami229@hotmail.com>
2011-05-30 16:32:36 +0900

    remove temp

--- edit/document/DocumentChangeHandler.java
+++
@@ -1,8 +0,0 @@
-package text.edit.document;
-
-public interface DocumentChangeHandler {
-    //ドキュメントが変更されたら、変更位置と変更後のテキストを渡す
-    void documentChanged(int row, int column, String text);
-    //ドキュメントが変更されたら、変更された行番号を渡す
-    void changeLine(int[] lines);
-}
